5a0df7d8f650fd24433a21b8d53380ebeb05db8f
Erinç Argımak <erar93@gmail.com>
2017-10-16 17:56:07 -0400

    add portfolio files

---
+++ main.js
@@ -0,0 +1,41 @@
+function adjustContainerHeight() {
+    $("#container").css("margin-top", $("header").css("height"));
+    var wh = $( window ).height();
+    var fh = parseFloat( $("footer").css("height") );
+    var mt = parseFloat( $("#container").css("margin-top") );
+    $("#container").height(wh - fh - mt);
+}
+
+var shown_section;
+
+$(function(){
+    // Adjust container height according to heights of header and footer
+    adjustContainerHeight();
+
+    // When page is loaded, show the 'about' section
+    $("section").hide();
+    $("#about").show("slow");
+    shown_section = "about";
+
+    // When the section links are pressed, show the requested section
+    $( ".section-shortcuts > ul > li > a" ).click(function()
+    {
+        if($(this).text() !== shown_section)
+        {
+            $("section").hide("slow");
+            $( "#"+$(this).text() ).show("slow");
+            shown_section = $(this).text();
+        }
+        else
+        {
+            var fs = $( "#"+shown_section ).css("font-size");
+            $( "#"+shown_section ).animate({fontSize: "24"}, "fast");
+            $( "#"+shown_section ).animate({fontSize: fs}, "fast");
+        }
+    });
+
+    // When the window is resized, re-adjust container height
+    $( window ).resize(function() {
+        adjustContainerHeight();
+    });
+});
